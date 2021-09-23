a8f33acb15ab9a7fe06966d69f346bef0e66ce60
Julian RaShaun Gay <jrg180004@utdallas.edu>
2021-09-23 16:12:17 -0500

    Wrapping up HP/Dmg controller

---
+++ Game Jam/Assets/Stats/CharStat.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class CharStat : MonoBehaviour
+{
+    public int maxHP = 100;
+    public int currHP {get; private set;}
+    public int maxSP = 25; //for magic consumption
+    public Stat dmg;
+    public Stat armor;
+
+    void Awake() {
+        currHP = maxHP;
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.T)) TakeDmg(10);
+    }
+
+    public void TakeDmg(int dmg) {
+        //dmg -= armor.getVal();
+        dmg = Mathf.Clamp(dmg, 0, int.MaxValue);
+        currHP -= dmg;
+
+        Debug.Log(transform.name + " takes " + dmg + " damage");
+
+        if (currHP <= 0) {
+            Die();
+        }
+    }
+
+    public virtual void Die() {
+        //Die in some way
+        //This method is meant to be overwritten
+        Debug.Log(transform.name + " died");
+    }
+}
---
+++ Game Jam/Assets/Stats/PlayerStats.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    // void OnEquipmentChanged(Equipment newItem, Equipment oldItem) {
+    //     if(newItem != null) {
+    //         armor.AddMod(newItem.armorMod);
+    //         dmg.AddMod(newItem.armorMod);
+    //     }
+
+    //     if(oldItem != null) {
+    //         armor.RemoveMod(newItem.armorMod);
+    //         dmg.RemoveMod(newItem.armorMod);
+    //     }
+    // }
+}
