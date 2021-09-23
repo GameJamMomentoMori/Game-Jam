a5179bc73388fafaf4034d30b4337e75dae222cc
asmith564 <aaronsmith0709@gmail.com>
2021-09-23 11:00:27 -0500

    Player Movement Expanded
    Added Gravity
    Adjusted Player Speed
    Adjusted Mouse Sensitivity Default
    Added Player Jump

--- Game Jam/Assets/PlayerMovement.cs
+++ Game Jam/Assets/PlayerMovement.cs
@@ -8,15 +8,45 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jump = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        //check if the player is on the ground to reset the gravity velocity
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        //apply gravity to the player object
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //gets the use input from the keyboard
         float PlayerX = Input.GetAxis("Horizontal");
         float PlayerZ = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * PlayerX + transform.forward * PlayerZ;
 
+        //Moves the character the certain amount times the speed and time for FPS normalization
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+
+            velocity.y = Mathf.Sqrt(jump * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
--- Game Jam/Assets/Scripts/Mouse_Look.cs
+++ Game Jam/Assets/Scripts/Mouse_Look.cs
@@ -14,16 +14,20 @@ public class Mouse_Look : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Locks the cursor to the screen
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Mouse movement for camera control 
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
+
+        //restricts how much the character can move the camera up or down
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
