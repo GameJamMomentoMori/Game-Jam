c849127472d2edfec1303642f6d2df33bc79c63c
Julian RaShaun Gay <jrg180004@utdallas.edu>
2021-09-24 13:51:42 -0500

    PlayerStat completed; Updated Prefab

--- Game Jam/Assets/Scripts/Stats/PlayerStats.cs
+++ Game Jam/Assets/Scripts/Stats/PlayerStats.cs
@@ -2,23 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour
+public class PlayerStats : CharStat
 {
     // Start is called before the first frame update
     void Start()
     {
-        //EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
     }
 
-    // void OnEquipmentChanged(Equipment newItem, Equipment oldItem) {
-    //     if(newItem != null) {
-    //         armor.AddMod(newItem.armorMod);
-    //         dmg.AddMod(newItem.armorMod);
-    //     }
+    void OnEquipmentChanged(Equipment newItem, Equipment oldItem) {
+        if(newItem != null) {
+            armor.AddMod(newItem.armorMod);
+            dmg.AddMod(newItem.armorMod);
+        }
 
-    //     if(oldItem != null) {
-    //         armor.RemoveMod(newItem.armorMod);
-    //         dmg.RemoveMod(newItem.armorMod);
-    //     }
-    // }
+        if(oldItem != null) {
+            armor.RemoveMod(newItem.armorMod);
+            dmg.RemoveMod(newItem.armorMod);
+        }
+    }
 }
