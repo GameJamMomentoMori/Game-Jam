34a5aa56ef1c46da87e8f093b2ba7ab3e1abcb49
Whalics <91243745+Whalics@users.noreply.github.com>
2021-09-25 13:12:01 -0500

    Wave Ctronoler

--- Game Jam/Assets/WaveSpawner.cs
+++ Game Jam/Assets/WaveSpawner.cs
@@ -6,14 +6,22 @@ public class WaveSpawner : MonoBehaviour
 
     public enum SpawnState {Spawning, Waiting, Counting}
 
+    //IANS CHANGES
+    //Wave enemy and enemy count vars are now arrays. can define 
+    //enemy types to spawn and enemy counts per enemy type
+    //creating a wave with mismatching array lengths returns an error and doesnt spawn
+    //a wave
+    
     //defining a wave through a seperate class
     //Name of wave, enemy input (can expand for multiple enemy types), Number of enemies, rate enemies spawn
     [System.Serializable]
     public class Wave
     {
         public string name;
-        public Transform enemy;
-        public int Enemies;
+        [Header("Enemy Prefabs to spawn")]
+        public Transform[] enemy;
+        [Header("Number of enemies per type")]
+        public int[] Enemies;
         public float rate; 
     }
 
@@ -24,6 +32,7 @@ public class WaveSpawner : MonoBehaviour
     private int nextWave = 0;
 
     public Transform[] spawnPoints;
+    //public GameObject[] enemies;
 
     //time between waves in seconds
     public float timeBetweenWaves = 5f;
@@ -118,26 +127,31 @@ public class WaveSpawner : MonoBehaviour
     //A method for spawning the waves specified in the list
     IEnumerator SpawnWave (Wave _wave)
     {
-        Debug.Log("Spawning Wave: " + _wave.name);
-        state = SpawnState.Spawning;
-
-        //Spawn enemies
-        for(int i = 0; i < _wave.Enemies; i++)
-        {
-            SpawnEmemy(_wave.enemy);
-
-            //Wait between enemy spawns based upon the rate of enemy spawn
-            yield return new WaitForSeconds(1f / _wave.rate);
+        if(_wave.enemy.Length != _wave.Enemies.Length){
+            Debug.LogError("Unable to Spawn Wave: " + _wave.name + ". Enemy types array and enemy count array sizes do not match.");    
         }
+        else if(_wave.enemy.Length == _wave.Enemies.Length){
+            Debug.Log("Spawning Wave: " + _wave.name);
+            state = SpawnState.Spawning;
 
-        Debug.Log("Done Spawning");
+            for(int h = 0; h < _wave.enemy.Length; h++){
+            //Spawn enemies
+                for(int i = 0; i < _wave.Enemies[h]; i++){
+                    SpawnEnemy(_wave.enemy[h]);
+            //Wait between enemy spawns based upon the rate of enemy spawn
+                    yield return new WaitForSeconds(1f / _wave.rate);
+                }
+            }
+
+            Debug.Log("Done Spawning");
 
         //Set the spawn state to waiting while the player kills the enemies
-        state = SpawnState.Waiting;
-        yield break;
+            state = SpawnState.Waiting;
+            yield break;
+        }
     }
 
-    void SpawnEmemy(Transform _enemy)
+    void SpawnEnemy(Transform _enemy)
     {
 
         //pick a random spawn point to spawn the enemy at
