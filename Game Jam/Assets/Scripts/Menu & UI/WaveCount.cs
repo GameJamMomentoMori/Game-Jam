f69eb609afb2527567a247150a7a007f458151aa
Whalics <91243745+Whalics@users.noreply.github.com>
2021-09-28 13:46:57 -0500

    Hud, Ranged, Spawning changes

--- Game Jam/Assets/Scripts/Menu & UI/WaveCount.cs
+++ Game Jam/Assets/Scripts/Menu & UI/WaveCount.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class WaveCount : MonoBehaviour
 {
-    Text wave;
+    public TMP_Text wave;
     public static int waveNumber;
 
     void Start()
     {
-        wave = GetComponent<Text>();
+        //wave = GetComponent<Text>();
     }
 
     public void NextWave()
     {
         waveNumber += 1;
-        wave.text = "Wave #" + waveNumber;
+        wave.text = waveNumber.ToString();
     }
 }
