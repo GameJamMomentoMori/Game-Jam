007a3d6367d2fa85278872431f7f81f511b91efc
Whalics <91243745+Whalics@users.noreply.github.com>
2021-09-29 03:58:26 -0500

    pause menu over cutscene

--- Game Jam/Assets/Scripts/Dialogue Scripts/FadeDialogTransition.cs
+++ Game Jam/Assets/Scripts/Dialogue Scripts/FadeDialogTransition.cs
@@ -3,25 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class FadeTransition : MonoBehaviour
+public class FadeDialogTransition : MonoBehaviour
 {
     // Start is called before the first frame updat
     // Update is called once per frame
     [SerializeField] Animator _animator;
     void Start(){
-        int b = SceneManager.GetActiveScene().buildIndex;
-        if(b == 1){
-            StartCoroutine(FadeOut());
-        }
+            StartCoroutine(FadeIn());
+        
     }
     public IEnumerator FadeIn(){
-        yield return new WaitForSeconds(0.5f);
-        this.gameObject.SetActive(true);
+        _animator.Play("FadeIn");
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(FadeOut());
+        
     }
     public IEnumerator FadeOut(){
         this.gameObject.SetActive(true);
         _animator.Play("FadeOut");
         yield return new WaitForSeconds(1.5f);
         this.gameObject.SetActive(false);
+        
+        
     }
 }
