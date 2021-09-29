bd5f0c55c4d6ada45f1433e82a9b3cbb30137ec9
Whalics <91243745+Whalics@users.noreply.github.com>
2021-09-29 02:02:24 -0500

    STOP PUSHING ARENA THANKS ILY

--- Game Jam/Assets/Scripts/Dialogue Scripts/DialogManager.cs
+++ Game Jam/Assets/Scripts/Dialogue Scripts/DialogManager.cs
@@ -22,6 +22,10 @@ public class DialogManager : MonoBehaviour
     public TextMeshProUGUI textDisplay;
     public Animator animator_dialogText;
     public Animator animator_dialogBox;
+    public Animator pp;
+    public Animator dp;
+    public Animator op;
+
     public AudioSource click;
 
     public AudioSource intro;
@@ -29,55 +33,28 @@ public class DialogManager : MonoBehaviour
 
     void Start()
     {
+        StartCoroutine(BoxIn());
         if(cutscene == 1){
-            sentences[0] = "Hey! You're up!";
-            sentences[1] = "You fell pretty hard over there. I saw the whole thing! I'm just glad I was here to help you out.";
-            sentences[2] = "Anyways, there's no easy way out of this place. If you want to make it back to the surface, you'd better get going!";
-            sentences[3] = "...Oh! And take this bag. It can only carry 3 things, but I'm sure it'll be useful!";
+            sentences = new string[7];
+            sentences[0] = "Hold, adventurer!  What business have you in the Lord Araphel’s castle?";
+            sentences[1] = "Well if you need to know, old-timer, I am on my way to conscript his aid. Because of all my traveling, I've caught some disease not a single doctor could get rid of.  I figured this Araphel character might be able to cure what ails me with one of his potions.";
+            sentences[2] = "You ought to know, traveler, that Araphel is not a kind man known for charity.  His abode is ripe with all manner of foul beasts and ghouls.  Nary a single warrior has emerged from the castle alive, and quite honestly, I doubt you will be the first.  I would recommend you turn away now.  Go home and greet Death with open arms in peace.";
+            sentences[3] = "Death?  Look at me old man!  I am the most capable fighter this rotten hamlet has ever seen!  There is not a scratch upon my armor nor a scar on my face.  Death has not so much as whispered my name aloud, let alone get anywhere near me!  Hell, if I saw Death, I would cut him down where he stood, the same as any other cur!";
+            sentences[4] = "With enthusiasm such as that, you will meet your end soon enough.  I will pray the beast that makes your end has the decency to do so quickly and cleanly.";
+            sentences[5] = "Save your prayers, old man.  I may just liberate this rat pit from its bastard of a lord.";
+            sentences[6] = "Poor, poor traveler";
         }
-        if(cutscene == 1){
-            sentences = new string[3];
-            sentences[0] = "You're getting better! Maybe try putting different stuff in that bag?";
-            sentences[1] = "Don't worry though. I'll always be around to rescue you!";
-        }
-        if(cutscene == 1){
-            sentences = new string[3];
-            sentences[0] = "You know... if you're ever down on health, try throwing a coin into that fountain.";
-            sentences[1] = "I'm sure you'll feel a lot better!";
-        }
-        if(cutscene == 1){
-            sentences = new string[3];
-            sentences[0] = "Did you meet the red things?";
-            sentences[1] = "They don't move, but those weird orbs pack a serious punch!";
-        }
-        if(cutscene == 1){
-            sentences = new string[4];
-             sentences[0] = "...";
-            sentences[1] = "You don't talk much, do you?";
-            sentences[2] = "I'm just glad I know how to patch you up. You're good as new!";
-        }
-        if(cutscene == 1){
-            sentences = new string[3];
-            sentences[0] = "You know, if you ignore all the weird creatures that try and kill you...";
-            sentences[1] = "It's actually pretty nice out there!";
-        }
-        if(cutscene == 1){
-            sentences = new string[2];
-            sentences[0] = "I've got no clue what those dumb coins do. No where seems to accept them!";
-        }
-        if(cutscene == 1){
-            sentences = new string[2];
-            sentences[0] = "Practice makes perfect!";
-            sentences[1] = "You don't even want to THINK about what my first trip to the surface looked like...";
-        }
-        if(cutscene == 1){
-            sentences = new string[3];
-            sentences[0] = "I sincerely dislike those stationary blue baffoons.";
-            sentences[1] = "I'm sure you've encountered them, no?";
-        }
-        if(cutscene == 1){
-            sentences = new string[2];
-            sentences[0] = "You're almost there... I can just feel it!";
+        if(cutscene == 2){
+            sentences = new string[9];
+            sentences[0] = "That should be… the last of them… Had me worried for a bit there… now to find Araphel...";
+            sentences[1] = "Well done traveler.  I suppose I misjudged you in a few places";
+            sentences[2] = "I have come to collect.  I congratulate you on your victory over Araphel’s forces, young heroine.  Even still, you should have listened to me before.  Now it is too late to greet me with open arms.";
+            sentences[3] = "Greet you?  What are you talking about?";
+            sentences[4] = "Come with me, young heroine.  You must accept what has come to pass.";
+            sentences[5] = "There is still time!  I just have to find the cure!  It has to be here among Araphel’s tonics…";
+            sentences[6] = "My afflictions have no cure.  Join me in the abyss.  There is no escape.";
+            sentences[7] = "I’ll not go quietly!  I’ve avoided you for this long.  If you want my life, you’ll have to take it!";
+            sentences[8] = "So be it. ";
         }
         Player = GameObject.Find("Player");
     }
@@ -93,10 +70,7 @@ public class DialogManager : MonoBehaviour
         if(sentenceComplete){
             if(Input.GetKeyDown("e")){
                 
-                sentenceComplete = false;
-                dialogBox.SetActive(true);
-                dialogText.SetActive(true);
-                NextSentence();
+               
             }
         }
     }
@@ -121,6 +95,17 @@ public class DialogManager : MonoBehaviour
         yield return new WaitForSeconds(0.2f);
         dialogBox.SetActive(false);
         dialogText.SetActive(false);
+    }
+    IEnumerator BoxIn(){
+        yield return new WaitForSeconds(2f);
+        animator_dialogBox.Play("SlideIn");
+        animator_dialogText.Play("SlideIn");
+        yield return new WaitForSeconds(1.5f);
+        op.Play("PortraitIn");
+        yield return new WaitForSeconds(0.7f);
+         sentenceComplete = false;
+                NextSentence();
+
     }
     IEnumerator Type(){
         foreach (char letter in sentences[index].ToCharArray()){ 
