228823f6e1063911b35f803dd887f33be178bab2
Julian RaShaun Gay <jrg180004@utdallas.edu>
2021-09-29 05:21:04 -0500

    Update WaveSpawner.cs

--- Game Jam/Assets/Scripts/WaveSpawner.cs
+++ Game Jam/Assets/Scripts/WaveSpawner.cs
@@ -7,7 +7,6 @@ public class WaveSpawner : MonoBehaviour
     public enum SpawnState {Spawning, Waiting, Counting}
     public WaveCount WCount;
     public GameObject WaveCountOBJ;
-    public DialogManager dialog;
     public GameObject black;
 
     //IANS CHANGES
@@ -137,10 +136,6 @@ public class WaveSpawner : MonoBehaviour
     //A method for spawning the waves specified in the list
     IEnumerator SpawnWave (Wave _wave)
     {
-        if (!dialog.dialogDone) {
-            yield return new WaitForSeconds(10f);
-        }
-
         if(_wave.enemy.Length != _wave.Enemies.Length){
             Debug.LogError("Unable to Spawn Wave: " + _wave.name + ". Enemy types array and enemy count array sizes do not match.");    
         }
